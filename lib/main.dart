dd7b8cfa97594836f5786206065fe579ec51e3ec
MalingasCUMBANE <malingascumbane1@gmail.com>
2023-02-11 16:48:57 +0200

    update daily message

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Built by Malingas Cumbane'),
+      home: const MyHomePage(title: 'Flutterando'),
     );
   }
 }
